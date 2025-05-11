7116bb3b88a113eb483f23cd0361b4c4c55e0aaa
Mohammed N <maado@mohammednur.com>
2025-05-11 20:39:48 +0400

    chore: changed the swagger api naming

--- CC.Presentation/Program.cs
+++ CC.Presentation/Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Conversion API", Version = "v1" });
 
     var jwtSecurityScheme = new OpenApiSecurityScheme
     {
